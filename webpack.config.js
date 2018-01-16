dd7347a835930937eda0aeb6fa7c549c7ee3bfb8
Jonathan Israel Pichardo Ortega <jonathan.pichardo@softtek.com>
2018-01-16 12:37:52 -0800

    Webpack + Babel

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app/app.js',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /(node_modules)/
+        }]
+    }
+}
