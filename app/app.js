1b5ff4f854bb6fe785a8cdaf7934a434e2cc6d66
Jonathan Israel Pichardo Ortega <jonathan.pichardo@softtek.com>
2018-01-16 14:16:39 -0800

    API KEY

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 import ReactDom from 'react-dom';
 import React, { Component } from 'react';
 
+const API_KEY = '171a7c48da0aaede9e3a66ef3be2f483';
+
 export class App extends Component {
 
     constructor(props) {
